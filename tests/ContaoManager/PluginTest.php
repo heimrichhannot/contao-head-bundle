c000a5abfdcbc2c3b62c6f5e64a89da2d8887242
Martin Kunitzsch <m.kunitzsch@heimrich-hannot.de>
2018-03-12 16:40:56 +0100

    added tests

--- tests/ContaoManager/PluginTest.php
+++ tests/ContaoManager/PluginTest.php
@@ -14,6 +14,12 @@ use Contao\ManagerPlugin\Bundle\Parser\DelegatingParser;
 use Contao\TestCase\ContaoTestCase;
 use HeimrichHannot\HeadBundle\ContaoManager\Plugin;
 use HeimrichHannot\HeadBundle\HeimrichHannotContaoHeadBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Test the plugin class
@@ -43,5 +49,39 @@ class PluginTest extends ContaoTestCase
         static::assertInstanceOf(BundleConfig::class, $bundles[0]);
         static::assertEquals(HeimrichHannotContaoHeadBundle::class, $bundles[0]->getName());
         static::assertEquals([ContaoCoreBundle::class], $bundles[0]->getLoadAfter());
+    }
+
+    public function testGetRouteCollection()
+    {
+        define('TL_ROOT', 'src');
+
+        $plugin = new Plugin();
+
+        $kernel = $this->createMock(KernelInterface::class);
+        $resolver = $this->getMockBuilder(LoaderResolverInterface::class)->getMock();
+        $resolvedLoader = $this->getMockBuilder(LoaderInterface::class)->getMock();
+
+        $resolver->expects($this->once())
+            ->method('resolve')
+            ->will($this->returnValue($resolvedLoader));
+
+        $originalRoute = new Route(TL_ROOT.'/Resources/config/routing.yml');
+        $expectedCollection = new RouteCollection();
+        $expectedCollection->add('one_test_route', $originalRoute);
+        $expectedCollection->addResource(new FileResource(TL_ROOT.'/Resources/config/routing.yml'));
+
+        $resolvedLoader
+            ->expects($this->once())
+            ->method('load')
+            ->will($this->returnValue($expectedCollection));
+
+        $loader = $this->getMockBuilder(LoaderInterface::class)->getMock();
+        $loader->expects($this->any())
+            ->method('getResolver')
+            ->will($this->returnValue($resolver));
+
+        $collection = $plugin->getRouteCollection($resolver, $kernel);
+
+        $this->assertInstanceOf(RouteCollection::class, $collection);
     }
 }
