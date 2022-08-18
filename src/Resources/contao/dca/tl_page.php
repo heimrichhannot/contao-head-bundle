fb4e84597d23fd3f7993e413f103c87cafce29e7
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-08-18 09:15:10 +0200

    fixed tl_page palette integration, added fields to regular pages

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,51 +1,77 @@
 <?php
-/**
- * Copyright (c) 2017 Heimrich & Hannot GmbH
- * @author Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
- * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+
+/*
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
  */
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 $dca = &$GLOBALS['TL_DCA']['tl_page'];
 
-/**
+/*
  * palettes
  */
-$dca['palettes']['root'] = str_replace(';{sitemap_legend', ';{head_legend},addHeadDefaultImage;{head_twitter_legend},twitterSite;{sitemap_legend', $dca['palettes']['root']);
-if (isset($dca['palettes']['rootfallback'])) {
-    $dca['palettes']['rootfallback']           = str_replace(';{sitemap_legend', ';{head_legend},addHeadDefaultImage;{head_twitter_legend},twitterSite;{sitemap_legend', $dca['palettes']['rootfallback']);
-}
+PaletteManipulator::create()
+    ->addField(
+        'addHeadDefaultImage',
+        'meta_legend',
+        PaletteManipulator::POSITION_APPEND
+    )
+    ->addField(
+        'twitterSite',
+        'meta_legend',
+        PaletteManipulator::POSITION_APPEND
+    )
+    ->applyToPalette('root', 'tl_page')
+    ->applyToPalette('rootfallback', 'tl_page');
 
-/**
+PaletteManipulator::create()
+    ->addField(
+        'addHeadDefaultImage',
+        'meta_legend',
+        PaletteManipulator::POSITION_APPEND
+    )
+    ->addField(
+        'twitterSite',
+        'meta_legend',
+        PaletteManipulator::POSITION_APPEND
+    )
+    ->applyToPalette('root', 'tl_page')
+    ->applyToPalette('regular', 'tl_page')
+    ->applyToPalette('rootfallback', 'tl_page');
+
+/*
  * Subpalettes
  */
 $dca['palettes']['__selector__'][] = 'addHeadDefaultImage';
 $dca['subpalettes']['addHeadDefaultImage'] = 'headDefaultImage';
 
 /**
- * fields
+ * fields.
  */
-
 $fields = [
     'addHeadDefaultImage' => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_page']['addHeadDefaultImage'],
-        'exclude'   => true,
+        'label' => &$GLOBALS['TL_LANG']['tl_page']['addHeadDefaultImage'],
+        'exclude' => true,
         'inputType' => 'checkbox',
-        'eval'      => ['submitOnChange' => true],
-        'sql'       => "char(1) NOT NULL default ''"
+        'eval' => ['submitOnChange' => true, 'tl_class' => 'w50 clr'],
+        'sql' => "char(1) NOT NULL default ''",
     ],
-    'headDefaultImage'    => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_page']['headDefaultImage'],
-        'exclude'   => true,
+    'headDefaultImage' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_page']['headDefaultImage'],
+        'exclude' => true,
         'inputType' => 'fileTree',
-        'eval'      => ['fieldType' => 'radio', 'filesOnly' => true, 'extensions' => 'jpg,jpeg,png', 'mandatory' => true],
-        'sql'       => "binary(16) NULL"
+        'eval' => ['fieldType' => 'radio', 'filesOnly' => true, 'extensions' => 'jpg,jpeg,png', 'mandatory' => true],
+        'sql' => 'binary(16) NULL',
     ],
-    'twitterSite'         => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_page']['twitterSite'],
+    'twitterSite' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_page']['twitterSite'],
         'inputType' => 'text',
-        'exclude'   => true,
-        'eval'      => ['tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL DEFAULT ''",
+        'exclude' => true,
+        'eval' => ['tl_class' => 'w50 clr'],
+        'sql' => "varchar(255) NOT NULL DEFAULT ''",
     ],
 ];
 
