44b32ff529f7b74d5894840f5bea3cccda4e0ea7
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-06-16 16:03:56 +0200

    remove service in contao 4.13

--- src/DependencyInjection/HeimrichHannotHeadBundleExtension.php
+++ src/DependencyInjection/HeimrichHannotHeadBundleExtension.php
@@ -1,13 +1,15 @@
 <?php
 
 /*
- * Copyright (c) 2022 Heimrich & Hannot GmbH
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
 
 namespace HeimrichHannot\HeadBundle\DependencyInjection;
 
+use Contao\CoreBundle\EventListener\DataContainer\DisableCanonicalFieldsListener;
+use HeimrichHannot\HeadBundle\EventListener\CanonicalListener;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 
@@ -18,6 +20,10 @@ class HeimrichHannotHeadBundleExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         $container->setParameter('huh_head', $config);
+
+        if (class_exists(DisableCanonicalFieldsListener::class)) {
+            $container->removeDefinition(CanonicalListener::class);
+        }
     }
 
     public function getAlias()
