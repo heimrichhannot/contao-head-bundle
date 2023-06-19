b8f6569c6e5cfe51d8146a8f0373962cb140e3dc
Thomas <koertho@users.noreply.github.com>
2023-06-19 10:51:56 +0200

    Port 4.13 canonicaly functionality (#10)
    * add link tag support
    
    * use HtmlHeadTagManager for canonical urls, deprecated old tags
    
    * cleanup and more deprecations
    
    * support legacy way adding link tags
    
    * port canonicals functionality from contao 4.13
    
    * add translations, fix backend issue
    
    * remove service in contao 4.13
    
    * fixed language
    
    * fix container for contao 4.13

---
+++ src/EventListener/CanonicalListener.php
@@ -0,0 +1,168 @@
+<?php
+
+/*
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\HeadBundle\EventListener;
+
+use Contao\Controller;
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\CoreBundle\ServiceAnnotation\Callback;
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\DataContainer;
+use Contao\LayoutModel;
+use Contao\PageModel;
+use Contao\PageRegular;
+use HeimrichHannot\HeadBundle\Manager\HtmlHeadTagManager;
+use HeimrichHannot\UtilsBundle\Util\Utils;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Backports canonical link functionality of contao 4.13 to contao 4.9.
+ */
+class CanonicalListener
+{
+    private Utils $utils;
+    private HtmlHeadTagManager $headTagManager;
+    private RequestStack $requestStack;
+
+    public function __construct(Utils $utils, HtmlHeadTagManager $headTagManager, RequestStack $requestStack)
+    {
+        $this->utils = $utils;
+        $this->headTagManager = $headTagManager;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @Hook("loadDataContainer")
+     */
+    public function addFields(string $table): void
+    {
+        if (PageModel::getTable() !== $table) {
+            return;
+        }
+
+        $dca = &$GLOBALS['TL_DCA'][$table];
+
+        $dca['fields']['enableCanonical'] = [
+            'exclude' => true,
+            'inputType' => 'checkbox',
+            'default' => true,
+            'eval' => ['tl_class' => 'w50 m12'],
+            'sql' => "char(1) NOT NULL default ''",
+        ];
+        $dca['fields']['canonicalLink'] = [
+            'exclude' => true,
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['rgxp' => 'url', 'decodeEntities' => true, 'maxlength' => 255, 'dcaPicker' => true, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ];
+        $dca['fields']['canonicalKeepParams'] = [
+            'exclude' => true,
+            'inputType' => 'text',
+            'eval' => ['decodeEntities' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ];
+
+        PaletteManipulator::create()
+            ->addLegend('canonical_legend', 'meta_legend', PaletteManipulator::POSITION_AFTER, true)
+            ->addField('canonicalLink', 'canonical_legend', PaletteManipulator::POSITION_APPEND)
+            ->addField('canonicalKeepParams', 'canonical_legend', PaletteManipulator::POSITION_APPEND)
+            ->applyToPalette('regular', 'tl_page');
+
+        PaletteManipulator::create()
+            ->addField('enableCanonical', 'adminEmail', PaletteManipulator::POSITION_BEFORE)
+            ->applyToPalette('root', 'tl_page')
+            ->applyToPalette('rootfallback', 'tl_page');
+    }
+
+    /**
+     * @Callback(table="tl_page", target="fields.canonicalLink.load")
+     * @Callback(table="tl_page", target="fields.canonicalKeepParams.load")
+     */
+    public function disableCanonicalFieldsListener(string $value, DataContainer $dc): string
+    {
+        if (!$dc->id || ($pageModel = PageModel::findByPk($dc->id)) === null) {
+            return $value;
+        }
+
+        if (($rootPageModel = $this->utils->request()->getCurrentRootPageModel($pageModel)) === null) {
+            return $value;
+        }
+
+        if (!$rootPageModel->enableCanonical) {
+            $GLOBALS['TL_DCA'][$dc->table]['fields'][$dc->field]['eval']['disabled'] = true;
+        }
+
+        return $value;
+    }
+
+    /**
+     * @Hook("generatePage", priority=-9)
+     */
+    public function addCanonicalIfNotSet(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    {
+        $rootPageModel = $this->utils->request()->getCurrentRootPageModel($pageModel);
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (!$request || !$rootPageModel->enableCanonical || $this->headTagManager->getCanonical()) {
+            return;
+        }
+
+        if ($pageModel->canonicalLink) {
+            $url = Controller::replaceInsertTags($pageModel->canonicalLink);
+
+            $mainRequest = null;
+
+            if (method_exists($this->requestStack, 'getMainRequest')) {
+                $mainRequest = $this->requestStack->getMainRequest();
+            } else {
+                $mainRequest = $this->requestStack->getMasterRequest();
+            }
+
+            // Ensure absolute links
+            if (!preg_match('#^https?://#', $url)) {
+                if (!$mainRequest) {
+                    throw new \RuntimeException('The request stack did not contain a request');
+                }
+
+                $url = $request->getSchemeAndHttpHost().$request->getBasePath().'/'.$url;
+            }
+
+            $this->headTagManager->setCanonical($url);
+
+            return;
+        }
+
+        $keepParams = [];
+
+        if ($pageModel->canonicalKeepParams) {
+            $keepParams = array_map('trim', explode(',', $pageModel->canonicalKeepParams));
+        }
+
+        $params = [];
+
+        foreach ($request->query->all() as $originalParam => $value) {
+            foreach ($keepParams as $param) {
+                $regex = sprintf('/^%s$/', str_replace('\*', '.*', preg_quote($param, '/')));
+
+                if (preg_match($regex, $originalParam)) {
+                    $params[$originalParam] = $value;
+                }
+            }
+        }
+
+        $request = Request::create(
+            $request->getSchemeAndHttpHost().$request->getBaseUrl().$request->getPathInfo(),
+            $request->getMethod(),
+            $params
+        );
+
+        $this->headTagManager->setCanonical($request->getUri());
+    }
+}
--- src/Resources/contao/languages/en/tl_page.php
+++ src/Resources/contao/languages/en/tl_page.php
@@ -1,21 +1,32 @@
 <?php
-/**
- * Copyright (c) 2017 Heimrich & Hannot GmbH
- * @author Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
- * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+
+/*
+ * Copyright (c) 2023 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
  */
 
 $lang = &$GLOBALS['TL_LANG']['tl_page'];
 
-/**
+/*
  * Fields
  */
 $lang['addHeadDefaultImage'] = ['Add fallback image for the meta tags', 'This image is used for og:image and twitter:image.'];
 $lang['headDefaultImage'] = ['Fallback image (at least 200x200)', 'Choose an image here. Only PNG and JPG files allowed.'];
 $lang['twitterSite'] = ['Twitter @username', 'The Twitter @username a twitter card should be attributed to (twitter:site attribute).'];
 
-/**
+/*
  * Legends
  */
 $lang['head_legend'] = 'Head-Bundle';
 $lang['head_twitter_legend'] = 'Head-Bundle - Twitter';
+
+if (version_compare(VERSION, '4.13', '<')) {
+    $lang['canonical_legend'] = 'Canonical URL';
+    $lang['enableCanonical'][0] = 'Enable rel="canonical"';
+    $lang['enableCanonical'][1] = 'Add rel=&quot;canonical&quot; tags to the website.';
+    $lang['canonicalLink'][0] = 'Custom URL';
+    $lang['canonicalLink'][1] = 'Here you can set a custom canonical URL like https://example.com/.';
+    $lang['canonicalKeepParams'][0] = 'Query parameters';
+    $lang['canonicalKeepParams'][1] = 'By default, Contao strips the query parameters in the canonical URL. Here you can add a comma-separated list of query parameters to preserve. Use &quot;*&quot; as a wildcard.';
+}
